9f9296a29576bce036a0d2c6e1323db81745723f
Razzi65 <rizmoh65@gmail.com>
2022-10-06 21:45:00 +0500

    8 Qs done

--- index.ts
+++ index.ts
@@ -48,11 +48,18 @@
 
 // 4. Write a ts program to find maximum and minimum element in an array.
 
-// var a:number[] = [5,2,3,4,1,6,34,546,778,896,98,76,88]
+// var a:number[] = [5,2,3,4,1,6,34,88,101,77,89,76,88]
+// var b:number=0
 
-// for (var i=0;i<=a.length-1;i++){
-//     if (a[i] > a[i-1])
+// for (var i=0;i<=a.length;i++){
+//     for(var j=i+1;j<=a.length;j++) {
+//         if(a[i]>a[j]){
+//             b=a[i]          
+//         }
+//     }
 // }
+// console.log(b)
+
 
 
 
@@ -102,9 +109,32 @@
 // console.log("Total number of odd numbers are", c);
 
 // 7. Write a ts program to count total number of negative elements in an array.
+//     var a:number[] = [1,5,-6,8,-6,5,-14,-25,-9,45,87]
+//     var b:number = 0
+// for (var i=0;i<=a.length-1;i++) {
+//     if(a[i]<0) {
+//         b++
+//     }
+// }
+// console.log("Total negative number are", b);
 
 
 // 8. Write a ts program to copy all elements from an array to another array.
+// var a:number[] = [1,2,3,4]
+// var b:number[] = []
+
+// console.log(b)b;
+
+// var a:number[] = [1,2,3,4,5,6]
+// var b:number[] = [11,12,13,14,15,16]
+// var c:number[] = [21,22,23,24,15,16]
+// var e:number[] = [1,2,3,4,5,6]
+
+// var d = a.concat(b,c,e)
+// console.log(d);
+
+
+
 // 9. Write a ts program to insert an element in an array.
 // 10. Write a ts program to delete an element from an array at specified position.
 // 11. Write a ts program to count frequency of each element in an array.
