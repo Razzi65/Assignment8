ffe85d4787019677185dd6b468236375e63ba6d3
Razzi65 <rizmoh65@gmail.com>
2022-10-07 05:13:05 +0500

    17 done

--- index.ts
+++ index.ts
@@ -158,15 +158,149 @@
 
 
 // 10. Write a ts program to delete an element from an array at specified position.
+// var a:number[] = [1,2,3,4,5,6]
+
+// a.splice(2,3)
+// console.log(a);
+
+
+
 // 11. Write a ts program to count frequency of each element in an array.
+
+// var a:number[] = [1,2,3,4,5,4,2,1,9,5]
+// var b:number = 4
+// var c:number=0
+
+// for (var i=0;i<=a.length-1;i++) {
+//  for (var j=i+1;j<=a.length;j++) {
+//     if(a[i]==j[i]) {
+       
+        
+//     }
+//  }
+// }
+// console.log(c);
+
+
 // 12. Write a ts program to print all unique elements in the array.
+
+//NOT SOLVED//
+
+// var a:number [] = [1,2,3,4,5,6,5,44,8,3,2,1]
+// var b:number[] = []
+// var c:number = 0
+
+// for (var i=0;i<=a.length-1;i++) {
+//     for(var j=i+1;j<=a.length-1;j++) {
+//         if (a[i]==a[j]){
+//             b.push(a[i])
+//             c++
+         
+//         }
+//     } 
+// }
+// console.log(b);
+// console.log(c);
+
+
+
+
+
 // 13. Write a ts program to count total number of duplicate elements in an array.
+
+// var a:number[] = [1,2,3,4,3,2,1]
+// var b:number=0
+
+// for (var i=0;i<=a.length-1;i++){
+//     for(var j=i+1;j<=a.length-1;j++) {
+//         if(a[i]==a[j]){
+//             b++;
+            
+//         }
+//     }
+// }
+
+// console.log("Number of duplicate elements are",b);
+
+
+
 // 14. Write a ts program to delete all duplicate elements from an array.
+///** Not solved  *///
+// var a:number[] = [1,2,4,4,2,1]
+// console.log(a);
+
+// for(var i=0;i<=a.length-1;i++) {
+//     for(var j=i+1;j<=a.length-1;j++){
+//         if(a[i]==a[j]) {
+//             a.splice(a[i],a[j])
+            
+//         }
+//     }
+// }
+// console.log(a);
+
+
+
 // 15. Write a ts program to merge two array to third array.
+// var a:number[]=[1,2,3,4]
+// var b:number[]=[5,6,7,8]
+
+// var c:number[]=a.concat(b)
+// console.log(c);
+
+
 // 16. Write a ts program to find reverse of an array.
+
+// var a:number[]=[1,2,3,4]
+// var b:number[]=a.reverse()
+
+// console.log(b);
+
+
 // 17. Write a ts program to put even and odd elements of array in two separate array.
-// 18. Write a ts program to search an element in an array.
+
+// var a:number[] = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+// var evenn:number[]=[]
+// var oddd:number[]=[]
+
+// for (var i=0;i<=a.length-1;i++){
+//     if(a[i]%2==0){
+//         evenn.push(a[i])
+//     }
+//     else {
+//         oddd.push(a[i])
+//     }
+// }
+
+// console.log(evenn,oddd);
+
+
+
+// 18. Write a ts program to search an element in an array. (Why answer has undefined)
+
+// var a:number[] = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+// var aa = (bb:number) => {
+//     for(var i=0;i<=a.length-1;i++){
+//         if(bb==a[i]){
+            
+//             return console.log(a.indexOf(i+1));
+            
+//         }
+//     }
+// }
+
+// console.log(aa(6));
+
+
 // 19. Write a ts program to sort array elements in ascending or descending order.
+
+// var a:number[] = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+// a.sort(c,d) => (c-d)
+
+
 // 20. Write a ts program to sort even and odd elements of array separately.
 // 21. Write a ts program to left rotate an array.
 // 22. Write a ts program to right rotate an array.
